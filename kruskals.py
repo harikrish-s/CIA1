0ff9fe1d79acee674ee3c94019c7fc657b2c22c0
harikrish-s <71437366+harikrish-s@users.noreply.github.com>
2023-01-18 12:00:41 +0530

    Update kruskals.py

--- kruskals.py
+++ kruskals.py
@@ -1,2 +1,17 @@
 import heapq
 
+def krusals(graph,start):
+
+vertex = int(input("Enter the no.of vertices : "))
+edge = int(input("Enter the no.of edges : "))
+graph = {i: [] for i in range(vertex)}
+for i in range(edge):
+    print("\n===== EDGE "+ str(i) +" =====")
+    src = int(input("Enter the source node : "))
+    dest = int(input("Enter the destination node : "))
+    weight = int(input("Enter the weight of the edge : ")) 
+    graph[src].append((dest,weight))
+print("\n==========")
+start = int(input("Enter the starting vertex : "))
+output = prims(graph,start)
+print("Kruskals : ")
